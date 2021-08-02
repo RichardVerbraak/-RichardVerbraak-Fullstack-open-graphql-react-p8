ed483854b9e725533073b8b77538fecc4b391899
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-08-02 17:25:00 +0200

    Created userSchema type definitions

--- backend/schema/types/userSchema.js
+++ backend/schema/types/userSchema.js
@@ -10,6 +10,15 @@ const userTypeDefs = gql`
 	type Token {
 		value: String!
 	}
+
+	type Query {
+		me: User
+	}
+
+	type Mutation {
+		createUser(username: String!): User
+		login(username: String!, password: String!): Token
+	}
 `
 
 module.exports = userTypeDefs
