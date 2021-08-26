60320c11fbcec5f8512ec3841842bc7e970a11dc
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-08-26 16:46:28 +0200

    Added the necessary changes to have Apollo client working with websockets and thus subscriptions

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,12 @@ import {
 	HttpLink,
 	InMemoryCache,
 	ApolloProvider,
+	split,
 } from '@apollo/client'
+
+// For making subscriptions work
+import { getMainDefinition } from '@apollo/client/utilities'
+import { WebSocketLink } from '@apollo/client/link/ws'
 
 // Adds the authorization header(s) to the ApolloClient with the token value with every GQL request
 const authLink = setContext((_, { headers }) => {
@@ -27,11 +32,32 @@ const authLink = setContext((_, { headers }) => {
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
+// Websocket link
+const wsLink = new WebSocketLink({
+	uri: `ws://localhost:4000/graphql`,
+	options: {
+		reconnect: true,
+	},
+})
+
+const splitLink = split(
+	({ query }) => {
+		const definition = getMainDefinition(query)
+
+		return (
+			definition.kind === 'OperationDefinition' &&
+			definition.operation === 'subscription'
+		)
+	},
+	wsLink,
+	authLink.concat(httpLink)
+)
+
 // Concatenate the headers so httpLink can read/use them
 // Spread out both objects into one single objects (not this > {...authLInk, httpLink})
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
-	link: { ...authLink, ...httpLink },
+	link: splitLink,
 })
 
 // #### How to 'subscribe' to the client for responses
